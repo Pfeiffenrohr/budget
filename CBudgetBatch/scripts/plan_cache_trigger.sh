b3ce91d8d6e3594b5026985d8b5194d5c67f5238
Richard Lechner <richard@richardlechner.de>
2019-06-07 22:28:33 +0200

    pan CahceTrigger Memory geändert

--- CBudgetBatch/scripts/plan_cache_trigger.sh
+++ CBudgetBatch/scripts/plan_cache_trigger.sh
@@ -2,6 +2,6 @@
 cd /var/lib/cbudgetbatch
 while [ 1 -eq 1 ]
 do
-   /usr/bin/java -Xms512m  -Xmx1024m  -classpath /var/lib/cbudgetbatch/server.jar:/var/lib/cbudgetbatch/CbudgetBase-1.0.1-SNAPSHOT.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget budget trigger
+   /usr/bin/java  -classpath /var/lib/cbudgetbatch/server.jar:/var/lib/cbudgetbatch/CbudgetBase-1.0.1-SNAPSHOT.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget budget trigger
    sleep 300
 done
