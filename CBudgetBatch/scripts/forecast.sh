ea32c3c2c8cfcaeddf60e101af3540daa4306926
Richard Lechner <richard@richardlechner.de>
2019-10-30 20:36:34 +0100

    sleep angepasst

--- CBudgetBatch/scripts/forecast.sh
+++ CBudgetBatch/scripts/forecast.sh
@@ -3,5 +3,5 @@ cd /var/lib/cbudgetbatch
 while [ 1 -eq 1 ]
 do
    /usr/bin/java  -classpath /var/lib/cbudgetbatch/forecast.jar:/var/lib/cbudgetbatch/CbudgetBase-3.0.0.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.Forecast budget budget $connectstring
-   sleep 300
+   sleep 10000
 done
