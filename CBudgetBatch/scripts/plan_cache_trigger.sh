2bfcb2df82a01dac372a0f68fffb54eedd9406e0
Richard Lechner <richard@richardlechner.de>
2019-11-01 20:01:42 +0100

    Sleep ist nun variable

--- CBudgetBatch/scripts/plan_cache_trigger.sh
+++ CBudgetBatch/scripts/plan_cache_trigger.sh
@@ -3,5 +3,5 @@ cd /var/lib/cbudgetbatch
 while [ 1 -eq 1 ]
 do
    /usr/bin/java  -classpath /var/lib/cbudgetbatch/server.jar:/var/lib/cbudgetbatch/CbudgetBase-3.0.0.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget $connectstring trigger
-   sleep 300
+   sleep $cacheComputeIntervall
 done
