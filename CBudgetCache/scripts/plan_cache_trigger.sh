95d6982b6abfa580fc948d0250944b9baf6841bf
Lechner, Richard <lechnerri@mediamarktsaturn.com>
2020-02-18 10:07:48 +0100

    Compute only night

--- CBudgetCache/scripts/plan_cache_trigger.sh
+++ CBudgetCache/scripts/plan_cache_trigger.sh
@@ -2,6 +2,14 @@
 cd /var/lib/cbudgetbatch
 while [ 1 -eq 1 ]
 do
-   /usr/bin/java  -classpath /var/lib/cbudgetbatch/server.jar:/var/lib/cbudgetbatch/CbudgetBase-3.0.0.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget $connectstring trigger
-   sleep $cacheComputeIntervall
+    if [ $ComputeOnlyNight != "True" ]; then
+    	/usr/bin/java  -classpath /var/lib/cbudgetbatch/server.jar:/var/lib/cbudgetbatch/CbudgetBase-3.0.0.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget $connectstring trigger
+    else
+	    if [ `date +%H` -lt 21 ] && [ `date +%H` -gt 07 ]; then
+	        	echo "Time out of Range"
+	    else    	
+	   		/usr/bin/java  -classpath /var/lib/cbudgetbatch/server.jar:/var/lib/cbudgetbatch/CbudgetBase-3.0.0.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget $connectstring trigger
+	   	fi	
+	fi     
+    sleep $cacheComputeIntervall
 done
