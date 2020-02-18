76aa86846517256807af0c3ba4382cb884361d48
Lechner, Richard <lechnerri@mediamarktsaturn.com>
2020-02-18 11:24:47 +0100

    True zu Yes geändert

--- CBudgetCache/scripts/plan_cache_trigger.sh
+++ CBudgetCache/scripts/plan_cache_trigger.sh
@@ -2,7 +2,7 @@
 cd /var/lib/cbudgetbatch
 while [ 1 -eq 1 ]
 do
-    if [ $ComputeOnlyNight != "True" ]; then
+    if [ $ComputeOnlyNight != "Yes" ]; then
     	/usr/bin/java  -classpath /var/lib/cbudgetbatch/server.jar:/var/lib/cbudgetbatch/CbudgetBase-3.0.0.jar:/var/lib/cbudgetbatch/postgresql-42.2.4.jar  cbudgetbatch.BerechnePlanungBatch budget budget $connectstring trigger
     else
 	    if [ `date +%H` -lt 21 ] && [ `date +%H` -gt 07 ]; then
