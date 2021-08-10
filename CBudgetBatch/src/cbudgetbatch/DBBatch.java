86d3b30fa978e0717138d756ca0fe12b8a439f69
Lechner, Richard <lechnerri@mediamarktsaturn.com>
2021-08-10 07:53:52 +0200

    Forecast soll auch am heutigen Tag gelöscht werden

--- CBudgetBatch/src/cbudgetbatch/DBBatch.java
+++ CBudgetBatch/src/cbudgetbatch/DBBatch.java
@@ -352,7 +352,7 @@ public class DBBatch extends DB {
             //if (debug) System.out.println("insert into genre values(null,'"+genre+"') ");
             String stm_str="";
         
-                stm_str="delete from transaktionen where datum < '"+ datum +"' and name like ('%Forecast%') and planed='y'";
+                stm_str="delete from transaktionen where datum <= '"+ datum +"' and name like ('%Forecast%') and planed='y'";
             if (debug) System.out.println(stm_str);
             stmt = con.prepareStatement(stm_str);
             // if (debug) System.out.println("update data_"+jahr+" set temp_out="+temp+
